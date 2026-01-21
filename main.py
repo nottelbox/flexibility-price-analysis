f30757e0a865a09619218ddc92c13011f41b0c64
Simon Blum <simonblum@hotmail.de>
2026-01-21 13:44:07 +0100

    Add new endpoint for price summary

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("prices/summary")
+def summary():
+    return {
+        "average_price": 70,
+        "min_price": 50,
+        "max_price": 100,
+        "price_volatility": 5,
+    }
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
